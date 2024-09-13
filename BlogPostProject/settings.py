7018f1ea1fb1f3271b4ff4c562fdfb84b011354a
Kabilan <147981020+kabilan65@users.noreply.github.com>
2024-09-13 19:50:35 +0530

    settings.py

--- BlogPostProject/settings.py
+++ BlogPostProject/settings.py
@@ -83,26 +83,17 @@ WSGI_APPLICATION = 'BlogPostProject.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
+
+
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'database_name',          
-        'USER': 'database_user',
-        'PASSWORD': 'database_password',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
